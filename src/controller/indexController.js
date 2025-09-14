7c47ba7b6303746fd6bd67e1a3abed407db98ccf
Adit Syed Afnan <syedafnanadit@gmail.com>
2025-09-13 23:39:07 -0400

    tmp Folder Storing Enabled

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -3,12 +3,17 @@ const multer = require("multer");
 const path = require("path");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "..", "public", "uploads"));
+    const uploadDir = path.join("/tmp", "uploads");
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
+
 const upload = multer({ storage: storage });
 
 exports.getIndex = (req, res) => {
